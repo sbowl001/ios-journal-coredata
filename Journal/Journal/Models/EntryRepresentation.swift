05f84c97bc113d19878085199841a8bfdd5b9584
Stephanie Bowles <sbowl001@fiu.edu>
2019-07-17 10:10:05 +0800

    == function

---
+++ Journal/Journal/Models/EntryRepresentation.swift
@@ -0,0 +1,42 @@
+//
+//  EntryRepresentation.swift
+//  Journal
+//
+//  Created by Stephanie Bowles on 7/17/19.
+//  Copyright © 2019 Stephanie Bowles. All rights reserved.
+//
+
+import Foundation
+
+struct EntryRepresentation: Codable, Equatable {
+    var title: String
+    var bodyText: String?
+    var timestamp: Date?
+    var identifier: String?
+    var mood:  String?
+}
+
+
+func == (lhs: EntryRepresentation, rhs: Entry) -> Bool {
+    return lhs.identifier == rhs.identifier &&
+           lhs.title == rhs.title &&
+           lhs.timestamp == rhs.timestamp &&
+           lhs.bodyText == rhs.bodyText &&
+           lhs.mood == rhs.mood
+ 
+}
+
+func == (lhs: Entry, rhs: EntryRepresentation) -> Bool {
+    return rhs == lhs
+}
+
+func != (lhs: EntryRepresentation, rhs: Entry) -> Bool {
+    return !(rhs == lhs)
+}
+
+func != (lhs: Entry, rhs: EntryRepresentation) -> Bool {
+    return rhs != lhs
+}
+
+
+
